9fe9867f189db21966aafd53f857ec2a33af12d7
David <david.hdm9@gmail.com>
2021-03-02 12:27:04 +0100

    función abrir carrito hecha a medias

--- main.js
+++ main.js
@@ -2,14 +2,20 @@ let shoppingCartView = document.querySelector('#menu-second-view');
 let menuMainView = document.querySelector('#menu-first-view');
 
 function loadButtons(){
-    let btnEnviar = document.getElementById('enviar'); 
-    btnEnviar.addEventListener('click', openShoppingCart());
+    let openCart = document.getElementById('openCart');
+    let goBack = document.getElementById('goBack'); 
+    openCart.addEventListener('click', openShoppingCart);
+    goBack.addEventListener('click', closeShoppingCart);
 }
-
 
 function openShoppingCart(){
     shoppingCartView.classList.add('visible');
     menuMainView.classList.add('invisible');
+}
+
+function closeShoppingCart(){
+    shoppingCartView.classList.add('invisible');
+    menuMainView.classList.add('visible');
 }
 
 loadButtons();
